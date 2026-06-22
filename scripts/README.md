d1b0de683be0e98b6a0c3c09e630ad69f4ea8094
hrnn-dev <hrnn.dev@gmail.com>
2026-06-22 15:51:20 -0600

    refactor (infra): estructurar directorios de configuración y acorazar firewall de nodo karnak

---
+++ scripts/README.md
@@ -0,0 +1,19 @@
+#Citadel
+Infraestructure as code (IaC) para el despliegue y optimización de nodos handless Debian.
+
+##Descripción
+Este repositorio contiene la configuración, scripts de automatización y herramientas de orquestación para el 
+despliegue de 'Citadel', un nodo basado en Debian GNU/Linux 12.
+
+##Componentes de infraestructura.
+-**Sistema Operativo:** Debian 12 (Hardened Headless)
+-**Gestión: ** SSH, Tmux Multiplexing
+-**Herramientas de red: ** Nmap, Tcpdump, Scapy, etc.
+-**Entorno: ** Bash scripts para despliegue automatizado, Python, C, Rust.
+
+##Versionado
+Este laboratorio sigue una filosofía de versionado incremental. Cada script modificado es registrado para
+asegurar la reproducibilidad del entorno ante contingencias.
+
+-----
+Arquitectura by hrnn-dev
---
+++ scripts/deploy_qemu_sec.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#=================================================================
+# Aprovisionamiento Local: Nodo de pentesting con KVM/QEMU
+#================================================================
+
+VM_NAME="pentest-local-node"
+RAM_MB=4096
+VCPUS=2
+DISK_PATH="$HOME/Desarrollo/lab_local/vms/${VM_NAME}.qcow2"
+
+echo ">> Creando disco virtual de 10 GB en $DISK_PATH...."
+qemu-img create -f qcow2 $DISK_PATH 10G
+
+echo ">> Desplegando máquina virtual e iniciando instalador de Debian 12..."
+virt-install \
+    --connect qemu:///system \
+    --name $VM_NAME \
+    --memory $RAM_MB \
+    --vcpus $VCPUS \
+    --disk path=$DISK_PATH,format=qcow2 \
+    --os-variant debian12 \
+    --network network=default \
+    --location http://deb.debian.org/debian/dists/bookworm/main/installer-amd64/ \
+    --extra-args="console=tty0 console=ttyS0,115200n8" \
+    --graphics none
